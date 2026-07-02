342e3b5328faa2889ad51fad4d29dec289c71d9c
jaydendh <164262454+jaydendh@users.noreply.github.com>
2026-07-01 20:01:34 -0500

    Fixing windows nic data block

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "azurerm_linux_virtual_machine" "linux" {
   )
 }
 
-resource "azurerm_network" "windows" {
+resource "azurerm_network_interface" "windows" {
   name                = "nic-windows-${var.yourname}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
